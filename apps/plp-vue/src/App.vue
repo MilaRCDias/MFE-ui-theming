f9607c1653a9809585618745a5159b621de84a81
Mila R C D <milarabello.dias@gmail.com>
2022-12-21 11:35:51 +0100

    apps

---
+++ apps/plp-vue/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+<h1>this is a Vue.js MFE</h1>
+   <a href="/"> Back to Home</a>
+<!--  <Button> PLP vue </Button>
+ -->  </div>
+</template>
+
+<script setup>
+import Button from 'ui_lib/Button';
+
+</script>
